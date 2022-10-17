9fd68eac7b400e4160f6961524a17183f158d90a
hatanaka701 <ringo.lemon15@gmail.com>
2022-10-17 19:01:52 +0900

    add button name

---
+++ index.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+
+btn1.addEventListener('click', () => {
+
+});
