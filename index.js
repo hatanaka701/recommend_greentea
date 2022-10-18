55b24656236f361538deff91a385262ebfca5517
humanmodeling OIT <humanmodelingoit@humanmodelingnoMacBook-Air.local>
2022-10-18 17:50:54 +0900

    add result

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
 
-btn1.addEventListener('click', () => {
+let btn1flag = false;
 
+let result = document.getElementById('result');
+
+btn1.addEventListener('click', () => {
+    btn1flag = true
+    if (btn1flag) {
+        result.innerText = 'btn1が押されました';
+    }
 });
